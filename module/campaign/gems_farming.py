bbcaa30a75b3d7172045e9503e246bb85510cf5b
ReDawn <727299658@qq.com>
2024-08-02 13:58:51 +0800

    FIX?

--- module/campaign/gems_farming.py
+++ module/campaign/gems_farming.py
@@ -337,7 +337,7 @@ class GemsFarming(CampaignRun, Dock, EquipmentChange):
         if self.hard_mode:
             min_level = max(min_level, 49)
         scanner = ShipScanner(level=(min_level, max_level), emotion=(emotion, 150),
-                              fleet=self.config.Fleet_Fleet1, status='free')
+                              fleet=self._fleet_detail_enter, status='free')
         scanner.disable('rarity')
 
         self.dock_sort_method_dsc_set()
